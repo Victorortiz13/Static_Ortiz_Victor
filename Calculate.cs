2738d8798e9637d060ce4ec6bca7fa4d7d44dbe5
Victor <1victorjrortiz3@gmail.com>
2026-04-15 10:42:59 -0500

    everything!

---
+++ Calculate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Ortiz_Victor
+{
+    static class Calculate
+    {
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        // Intergers!
+        // Adds two intergers and returns the result
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        // Subtracts two intergers and returns the result
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        // Divides two intergers and returns the result
+        public static int Divi(int x, int y)
+        {
+            return x / y;
+        }
+        // Multiplies two intergers and returns the result
+        public static int Multi(int x, int y)
+        {
+            return x * y;
+        }
+
+        // Floats!
+        // Adds two floats and returns the result
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+        // Subtracts two floats and returns the result
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+        // Divides two floats and returns the result
+        public static float Divi(float x, float y)
+        {
+            return x / y;
+        }
+        // Multiplies two floats and returns the result
+        public static float Multi(float x, float y)
+        {
+            return x * y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Interger Ops!
+            Console.WriteLine("Interger Operations:");                              // Prints a message to the console
+            Console.WriteLine("Addition: 434 + 343 = " + Calculate.Add(434, 343));     // Calls the Add method in Calculate from the intergers and replaces the paramiters with new values and prints a message and the answer
+            Console.WriteLine("Subtraction: 67 - 69 = " + Calculate.Sub(67, 69));         // Calls the Sub method in Calculate from the intergers and replaces the paramiters with new values and prints a message and the answer
+            Console.WriteLine("Division: 61 / 41 = " + Calculate.Divi(61, 41));              // Calls the Divi method in Calculate from the intergers and replaces the paramiters with new values and prints a message and the answer
+            Console.WriteLine("Multiplication: 86 * 13 = " + Calculate.Multi(86, 13));          // Calls the Multi method in Calculate from the intergers and replaces the paramiters with new values and prints a message and the answer
+
+            // Float Ops!
+            Console.WriteLine("\nFloat Operations:");                                                   // Adds a line of space and prints a message to the console
+            Console.WriteLine("Addition: 434.334 + 343.443 = " + Calculate.Add(434.334f, 343.443f));       // Calls the Add method in Calculate from the floats and replaces the paramiters with new values and prints a message and the answer
+            Console.WriteLine("Subtraction: 67.7 - 69.9 = " + Calculate.Sub(67.7f, 69.9f));                   // Calls the Sub method in Calculate from the floats and replaces the paramiters with new values and prints a message and the answer
+            Console.WriteLine("Division: 61.1 / 41.4 = " + Calculate.Divi(61.6f, 41.4f));                        // Calls the Divi method in Calculate from the floats and replaces the paramiters with new values and prints a message and the answer
+            Console.WriteLine("Multiplication: 86.13 * 13.86 = " + Calculate.Multi(86.13f, 13.86f));                // Calls the Multi method in Calculate from the floats and replaces the paramiters with new values and prints a message and the answer
         }
-    }
+    } 
 }
